703f1007c74182be8e47ddad56a305aedf777c8f
vanquiche <stevevang2010@hotmail.com>
2022-12-16 13:21:24 +0900

    add fontawesome icon to app

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,38 @@ import App from "./App.vue";
 
 import "./assets/main.css";
 
-const app = createApp(App);
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-app.use(createPinia());
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-app.mount("#app");
+import {
+  faEdit,
+  faTrash,
+  faSquareCheck as faSquareCheckSolid,
+  faXmark,
+  faSquareXmark,
+  faSquarePen,
+  faRotateLeft,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { faSquareCheck, faSquare } from "@fortawesome/free-regular-svg-icons";
+
+library.add(
+  faEdit,
+  faSquareCheck,
+  faSquareCheckSolid,
+  faSquare,
+  faTrash,
+  faXmark,
+  faSquareXmark,
+  faSquarePen,
+  faRotateLeft
+);
+
+// app.use(createPinia());
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(createPinia())
+  .mount("#app");
