eeb266a09b03fb7e84b9cb9be7e781b32acc74eb
vanquiche <stevevang2010@hotmail.com>
2022-12-19 11:30:30 +0900

    add accent color

--- src/components/themes.ts
+++ src/components/themes.ts
@@ -1,20 +1,26 @@
 export const lightTheme: ThemeType = {
   bgColor: "white",
   color: "black",
-  inputBgColor: "rgb(230, 230, 230)",
+  inputBgColor: "rgb(220, 230, 230)",
+  disabledInputColor: "rgb(240, 240, 240)",
   switchBgColor: "black",
+  accentColor: "rgb(110, 112, 212)",
 };
 
 export const darkTheme: ThemeType = {
   bgColor: "black",
   color: "white",
   inputBgColor: "rgb(43, 43, 43)",
+  disabledInputColor: "rgb(20, 20, 20)",
   switchBgColor: "white",
+  accentColor: "rgb(110, 112, 212)",
 };
 
 export interface ThemeType {
   bgColor: string;
   color: string;
   inputBgColor: string;
+  disabledInputColor: string;
   switchBgColor: string;
+  accentColor: string;
 }
